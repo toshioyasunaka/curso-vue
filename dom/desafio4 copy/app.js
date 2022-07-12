134d5d0aaedf23d624fb857293e99782371eebda
Toshio <toshioyasunaka@hotmail.com>
2022-07-12 18:13:50 -0300

    refazendo desafio 4

--- dom/desafio4 copy/app.js
+++ dom/desafio4 copy/app.js
@@ -1,14 +1,28 @@
 new Vue({
 	el: '#desafio',
 	data: {
-
+		classe1: 'destaque',
+		perigo: true,
+		classe3: '',
+		classe4: '',
 	},
 	methods: {
 		iniciarEfeito() {
-
+			setInterval(() => {
+				this.classe1 == 'destaque' ? this.classe1 = 'encolher' : this.classe1 = 'destaque' 
+			}, 1000)
 		},
+
 		iniciarProgresso() {
 
+		},
+
+		setPerigo(event) {
+			if (event.target.value == 'true') {
+				this.perigo = true
+			} else if (event.target.value == 'false'){
+				this.perigo = false
+			}
 		}
 	}
 })
