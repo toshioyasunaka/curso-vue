5f5b849d73c19b43ad372d9150751039127ccc8b
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2022-07-13 20:41:13 -0300

    desafio 4 - testes

--- dom/desafio4 copy/app.js
+++ dom/desafio4 copy/app.js
@@ -1,26 +1,41 @@
-new Vue({
+new Vue ({
 	el: '#desafio',
 	data: {
 		classe1: 'destaque',
 		perigo: true,
 		classe3: '',
 		classe4: '',
+		estilo5: {
+			width: '100px',
+			height: '100px'
+		},
+		cor5: '',
+		progresso: 0,
+		concluido: '',
+		texto: '',
 	},
 	methods: {
 		iniciarEfeito() {
 			setInterval(() => {
-				this.classe1 == 'destaque' ? this.classe1 = 'encolher' : this.classe1 = 'destaque' 
-			}, 1000)
+				this.classe1 == 'destaque' ? this.classe1 = 'encolher' : this.classe1 = 'destaque';
+			}, 500)
 		},
-
 		iniciarProgresso() {
-
+			let valorProgresso = 0
+			let temporizador = setInterval(() => {
+				valorProgresso += 20;
+				this.progresso = `${valorProgresso}%`
+				if (valorProgresso === 100) {
+					clearInterval(temporizador);
+					this.concluido = {backgroundColor:'green'}
+					this.texto = 'Concluido!!'
+				}
+			}, 500)
 		},
-
-		setPerigo(event) {
-			if (event.target.value == 'true') {
+		atribuirClassePerigo(event) {
+			if (event.target.value === 'true') {
 				this.perigo = true
-			} else if (event.target.value == 'false'){
+			} else if (event.target.value === 'false') {
 				this.perigo = false
 			}
 		}
