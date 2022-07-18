24e688ac1827d67e751ebc6dce98904fb31d8dca
Toshio <toshioyasunaka@hotmail.com>
2022-07-18 17:09:58 -0300

    projeto 1

--- condicionais-e-listas/desafio-05/projeto-1/script.js
+++ condicionais-e-listas/desafio-05/projeto-1/script.js
@@ -16,39 +16,46 @@ new Vue ({
             this.gameHome = false;
             this.gameEnd = false;
             this.gameStarted = true;
+            this.battleHistoric = [];
             this.playerLife = 100;
             this.monsterLife = 100;
         },
 
         attack() {
-            const monsterDamage = Math.random() * (15 - 1);
-            const monsterIntegerDamage = monsterDamage.toFixed();
-            const playerDamage = Math.random() * (12 - 1);
-            const playerIntegerDamage = playerDamage.toFixed();
+            const monsterDamage = Math.random() * (15 - 1) + 1;
+            const monsterIntegerDamage = Math.round(monsterDamage);
+            const playerDamage = Math.random() * (12 - 1) + 1;
+            const playerIntegerDamage = Math.round(playerDamage);
 
             this.playerLife -= monsterIntegerDamage;
             this.monsterLife -= playerIntegerDamage;
+
+            this.registerBattle(`O jogador atacou o monstro e deu ${playerIntegerDamage} de dano.`, 'player-historic');
+            this.registerBattle(`O monstro atacou o jogador e deu ${monsterIntegerDamage} de dano.`, 'monster-historic');
 
             this.attackReactions();
         },
 
         specialAttack() {
-            const monsterDamage = Math.random() * (12 - 1);
-            const monsterIntegerDamage = monsterDamage.toFixed();
-            const playerDamage = Math.random() * (15 - 1);
-            const playerIntegerDamage = playerDamage.toFixed();
+            const monsterDamage = Math.random() * (12 - 1) + 1;
+            const monsterIntegerDamage = Math.round(monsterDamage);
+            const playerDamage = Math.random() * (15 - 1) + 1;
+            const playerIntegerDamage = Math.round(playerDamage);
 
             this.playerLife -= monsterIntegerDamage;
             this.monsterLife -= playerIntegerDamage;
+
+            this.registerBattle(`O jogador fez um ataque especial no monstro e deu ${playerIntegerDamage} de dano.`, 'player-historic');
+            this.registerBattle(`O monstro atacou o jogador e deu ${monsterIntegerDamage} de dano.`, 'monster-historic');
 
             this.attackReactions();
         },
 
         heal() {
-            const monsterDamage = Math.random() * (12 - 1);
-            const monsterIntegerDamage = monsterDamage.toFixed();
-            const playerHeal = Math.random() * (15 - 1);
-            const playerIntegerHeal = playerHeal.toFixed();
+            const monsterDamage = Math.random() * (12 - 1) + 1;
+            const monsterIntegerDamage = Math.round(monsterDamage);
+            const playerHeal = Math.random() * (15 - 1) + 1;
+            const playerIntegerHeal = Math.round(playerHeal);
 
             this.playerLife -= Number(monsterIntegerDamage);
             this.playerLife += Number(playerIntegerHeal);
@@ -57,6 +64,9 @@ new Vue ({
                 this.playerLife = 100;
                 alert("Sua vida está no máximo")
             }
+
+            this.registerBattle(`O jogador se curou em ${playerIntegerHeal} de vida.`, 'player-historic');
+            this.registerBattle(`O monstro atacou o jogador e deu ${monsterIntegerDamage} de dano.`, 'monster-historic');
 
             this.attackReactions();
         },
@@ -96,5 +106,9 @@ new Vue ({
             this.gameStarted = false;
             this.gameHome = true;
         },
+
+        registerBattle(text, styleClass) {
+            this.battleHistoric.unshift( {text, styleClass} ); //coloca o elemento na primeira posição da Array
+        }
     },
 })
