cabd15453bf097d0fc26fd8d2ff053b28387b48d
toshioyasunaka <toshioyasunaka@hotmail.com>
2022-07-23 10:37:34 -0300

    Components

---
+++ componentes/components-intro/src/Contador.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="contador">
+    <h1>Contadores</h1>
+    <span>{{ contador }}</span>
+    <button @click="add()">+</button>
+    <button @click="decrease()">-</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0,
+    };
+  },
+  methods: {
+    add() {
+      this.contador++;
+    },
+    decrease() {
+      this.contador--;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
