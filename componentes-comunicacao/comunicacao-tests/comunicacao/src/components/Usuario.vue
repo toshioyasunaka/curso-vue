d07fed90e498773320599c394edfb6ae95d3a3ac
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2022-07-25 09:34:03 -0300

    create comunicacao-tests

--- componentes-comunicacao/comunicacao-tests/comunicacao/src/components/Usuario.vue
+++ componentes-comunicacao/comunicacao-tests/comunicacao/src/components/Usuario.vue
@@ -2,11 +2,9 @@
     <div class="container">
         <h1>Componente Usuário</h1>
         <p>Esse é um componente muito legal!</p>
-        <p>Nome é <strong>{{ nome }}</strong></p>
-        <button @click="alterarNome()">Alterar Nome</button>
         <hr>
         <div class="componentes">
-            <app-usuario-info :nome="nome" @nomeMudou="nome = $event" :reiniciarFn= "reiniciarNome" />
+            <app-usuario-info />
             <app-usuario-editar />
         </div>
     </div>
@@ -17,20 +15,7 @@ import AppUsuarioInfo from './UsuarioInfo'
 import AppUsuarioEditar from './UsuarioEditar'
 
 export default {
-    components: { AppUsuarioInfo, AppUsuarioEditar },
-    data() {
-        return {
-            nome: 'Pedro',
-        }
-    },
-    methods: {
-        alterarNome() {
-          this.nome= 'Ana'
-        },
-        reiniciarNome() {
-          this.nome= 'Pedro'
-        }
-    }
+    components: { AppUsuarioInfo, AppUsuarioEditar }
 }
 </script>
 
--- componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
+++ componentes-comunicacao/comunicacao/src/components/UsuarioInfo.vue
@@ -2,12 +2,34 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
+        <p>Nome do usuário: <strong>{{ inverterNome() }}</strong></p>
+        <button @click="reiniciarNome()">Reiniciar Nome</button>
+        <button @click="reiniciarFn()">Reiniciar Nome (callback)</button>
     </div>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        nome: {
+            type: String,
+            //required: true,
+            //default: function() {
+            //    return Array(10).fill(0).join(',')
+            //}
+            default: "Anônimo",
+        },
+        reiniciarFn: Function
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        },
+        reiniciarNome() {
+            this.nome = 'Pedro'
+            this.$emit('nomeMudou', this.nome)
+        }
+    }
 }
 </script>
 
