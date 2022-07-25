ff72d960e82036827f94aa83388207aec057ada0
Toshio Yasunaka <toshioyasunaka@hotmail.com>
2022-07-25 08:07:28 -0300

    components upload

--- componentes/componente-intro/src/App.vue
+++ componentes/componente-intro/src/App.vue
@@ -1,10 +1,17 @@
 <template>
-  
+    <div id="app">
+        <h1>{{ contador }}</h1>
+    </div>
 </template>
 
 <script>
 export default {
-
+    data() {
+        return
+         {
+            contador: 0
+         }
+    }
 }
 </script>
 
